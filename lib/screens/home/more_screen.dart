be67abb9efef96ba3f93c0c4065c234068765e00
Aldwinny <aldwin0313@yahoo.com.ph>
2023-04-16 21:33:42 +0800

    Refactor Complete: Await testing and integration

--- lib/screens/home/more_screen.dart
+++ lib/screens/home/more_screen.dart
@@ -7,6 +7,7 @@ import 'package:foody_app/shared/colors.dart';
 import 'package:foody_app/utils/helper.dart';
 import 'package:foody_app/widgets/foody_navbar.dart';
 
+/// Refactor Complete
 class MoreScreen extends StatelessWidget {
   static const routeName = "/moreScreen";
   const MoreScreen({super.key});
@@ -22,85 +23,70 @@ class MoreScreen extends StatelessWidget {
               width: Helper.getScreenWidth(context),
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: SingleChildScrollView(
-                child: Column(children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "More",
-                        style: Helper.getTheme(context).headline5,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 100),
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20, top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "More",
+                            style: Helper.getTheme(context).headline5,
+                          ),
+                          Image.asset(
+                            Helper.getAssetName("cart.png", "virtual"),
+                          ),
+                        ],
                       ),
-                      Image.asset(
-                        Helper.getAssetName("cart.png", "virtual"),
+                    ),
+                    MoreCard(
+                      image: Image.asset(
+                        Helper.getAssetName("shopping_bag.png", "virtual"),
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  MoreCard(
-                    image: Image.asset(
-                      Helper.getAssetName("shopping_bag.png", "virtual"),
+                      name: "My Orders",
+                      handler: () {
+                        Navigator.of(context)
+                            .pushNamed(MyOrderScreen.routeName);
+                      },
                     ),
-                    name: "My Orders",
-                    handler: () {
-                      Navigator.of(context).pushNamed(MyOrderScreen.routeName);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  MoreCard(
-                    image: Image.asset(
-                      Helper.getAssetName("noti.png", "virtual"),
+                    MoreCard(
+                      image: Image.asset(
+                        Helper.getAssetName("noti.png", "virtual"),
+                      ),
+                      name: "Notifications",
+                      isNoti: true,
+                      handler: () {
+                        Navigator.of(context)
+                            .pushNamed(NotificationScreen.routeName);
+                      },
                     ),
-                    name: "Notifications",
-                    isNoti: true,
-                    handler: () {
-                      Navigator.of(context)
-                          .pushNamed(NotificationScreen.routeName);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  MoreCard(
-                    image: Image.asset(
-                      Helper.getAssetName("mail.png", "virtual"),
+                    MoreCard(
+                      image: Image.asset(
+                        Helper.getAssetName("mail.png", "virtual"),
+                      ),
+                      name: "Inbox",
+                      handler: () {
+                        Navigator.of(context).pushNamed(InboxScreen.routeName);
+                      },
                     ),
-                    name: "Inbox",
-                    handler: () {
-                      Navigator.of(context).pushNamed(InboxScreen.routeName);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  MoreCard(
-                    image: Image.asset(
-                      Helper.getAssetName("info.png", "virtual"),
+                    MoreCard(
+                      image: Image.asset(
+                        Helper.getAssetName("info.png", "virtual"),
+                      ),
+                      name: "About Us",
+                      handler: () {
+                        Navigator.of(context).pushNamed(AboutScreen.routeName);
+                      },
                     ),
-                    name: "About Us",
-                    handler: () {
-                      Navigator.of(context).pushNamed(AboutScreen.routeName);
-                    },
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                ]),
+                  ]),
+                ),
               ),
             ),
           ),
           const Positioned(
             bottom: 0,
-            left: 0,
             child: FoodyNavBar(
               more: true,
             ),
@@ -112,101 +98,73 @@ class MoreScreen extends StatelessWidget {
 }
 
 class MoreCard extends StatelessWidget {
-  const MoreCard({
-    super.key,
-    required this.name,
-    this.image,
-    this.isNoti = false,
-    this.handler,
-  });
+  const MoreCard(
+      {super.key,
+      required this.name,
+      this.image,
+      this.isNoti = false,
+      this.handler,
+      this.padding = const EdgeInsets.symmetric(horizontal: 5, vertical: 7)});
 
   final String name;
   final Image? image;
   final bool isNoti;
   final Function()? handler;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: handler,
-      child: SizedBox(
-        height: 70,
-        width: double.infinity,
-        child: Stack(
-          children: [
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-              margin: const EdgeInsets.only(
-                right: 20,
+    return Padding(
+      padding: padding,
+      child: Material(
+        color: AppColor.placeholder.withAlpha(50),
+        borderRadius: BorderRadius.circular(100),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(100),
+          onTap: handler,
+          child: Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                child: CircleAvatar(
+                    radius: 23,
+                    backgroundColor: AppColor.placeholder.withAlpha(250),
+                    child: image),
               ),
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                color: AppColor.placeholderBg,
-              ),
-              child: Row(
-                children: [
-                  Container(
-                      width: 50,
-                      height: 50,
-                      decoration: const ShapeDecoration(
-                        shape: CircleBorder(),
-                        color: AppColor.placeholder,
-                      ),
-                      child: image),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    name,
-                    style: const TextStyle(
-                      color: AppColor.primary,
+              Text(name),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        if (isNoti)
+                          const Padding(
+                            padding: EdgeInsets.only(right: 15),
+                            child: CircleAvatar(
+                              radius: 12,
+                              child: Center(
+                                child: Text(
+                                  "69",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12),
+                                ),
+                              ),
+                            ),
+                          ),
+                        CircleAvatar(
+                            radius: 16,
+                            backgroundColor: AppColor.placeholder.withAlpha(75),
+                            child: const Icon(Icons.arrow_forward_ios,
+                                size: 19, color: Colors.black))
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Container(
-                height: 30,
-                width: 30,
-                decoration: const ShapeDecoration(
-                  shape: CircleBorder(),
-                  color: AppColor.placeholderBg,
-                ),
-                child: const Icon(
-                  Icons.arrow_forward_ios_rounded,
-                  color: AppColor.secondary,
-                  size: 17,
-                ),
-              ),
-            ),
-            if (isNoti)
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  height: 20,
-                  width: 20,
-                  margin: const EdgeInsets.only(
-                    right: 50,
-                  ),
-                  decoration: const ShapeDecoration(
-                    shape: CircleBorder(),
-                    color: Colors.red,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "15",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              )
-          ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
